febf68b9cd5a86842a3e1b707c44977fac1a32ff
kelebosk-code <kelebosk@gmail.com>
2026-07-11 18:26:18 +0200

    Add offline support

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "bloom-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./logo.png",
+  "./flower.png",
+  "./butterfly.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./splash.png",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
